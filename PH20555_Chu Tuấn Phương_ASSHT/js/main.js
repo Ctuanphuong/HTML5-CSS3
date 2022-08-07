f9f8e10e410a90f90b86cebb02f19d53ac4fbe2f
Ctuanphuong18 <Ctuanphuong18@gmail.com>
2022-08-08 00:37:59 +0700

    first update code

---
+++ PH20555_Chu Tuấn Phương_ASSHT/js/main.js
@@ -0,0 +1,34 @@
+//vehicles
+var slideIndex = 0;
+showSlide();
+
+function plusSlides(n) {
+    showSlide2((slideIndex += n));
+}
+
+function showSlide2(n) {
+    var slides = document.getElementsByClassName("bus");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function showSlide() {
+    var slides = document.getElementsByClassName("bus");
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 3000);
+}
